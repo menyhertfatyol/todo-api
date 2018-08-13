dfa4eda43c6e92a0e02199cb8646ad9ddc93332d
Menyhert Fatyol <mfatyol@emarsys.com>
2018-08-13 08:25:18 +0200

    Fix factorybor request spec

--- spec/requests/todos_spec.rb
+++ spec/requests/todos_spec.rb
@@ -1,13 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe "Todo API", type: :request do
-  let(:todos) { create_list(:todo, 10) }
+  let!(:todos) { create_list(:todo, 10) }
   let(:todo_id) { todos.first.id }
 
   describe 'GET /todos' do
     before { get '/todos' }
 
     it 'returns todos' do
+      p todos
       expect(response.body).not_to be_empty
     end
   end
