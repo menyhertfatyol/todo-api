4ac3c7a683b74a5087503fbe25cb58cdbc9f6f58
Menyhert Fatyol <mfatyol@emarsys.com>
2018-08-15 21:10:09 +0200

    adds tests to GET /todos/:id endpoint, adds helper methods to request specs to make the code readable

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,6 +1,11 @@
 class TodosController < ApplicationController
   def index
     @todos = Todo.all
-    render @todos
+    render json: @todos
+  end
+
+  def show
+    @todo = Todo.find(params[:id])
+    render json: @todo
   end
 end
--- spec/requests/todos_spec.rb
+++ spec/requests/todos_spec.rb
@@ -8,10 +8,21 @@ RSpec.describe "Todo API", type: :request do
     before { get '/todos' }
 
     it 'returns todos' do
-      p todos
-      expect(response.body).not_to be_empty
+      expect(json_body).not_to be_empty
+      expect(json_body.size).to eq(10)
+    end
+
+    it 'returns status code 200' do
+      expect(response).to have_http_status(200)
     end
   end
 
+  describe 'GET /todos/:id' do
+    before { get "/todos/#{todo_id}" }
 
+    it 'returns specific todo' do
+      expect(json_body).not_to be_empty
+      expect(json_body['id']).to eq(todo_id)
+    end
+  end
 end
